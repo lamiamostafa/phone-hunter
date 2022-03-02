8a6c103fe903bde5624cf7af78f197f43fec2113
lamiamostafa <lamiamostafa791@gmail.com>
2022-03-02 14:57:09 +0600

    mobile responsive added

--- js/app.js
+++ js/app.js
@@ -78,27 +78,42 @@ const displayPhoneDetail = data => {
         document.getElementById('release').style.display = 'none';
 
     }
+    console.log(data.others ? data.others : "");
+
+    // if (data.others.WLAN == '' || data.others.GPS == ''
+    //     || data.others.Bluetooth == '' || data.others.NFC == '' || data.others.USB == '' || data.others.Radio == '') {
+    //     document.getElementById('others').style.textContent = 'No found'
+    // }
+
+
     div.innerHTML = `
     
     
     <div class="col-lg-12 ">
         <div class="  card-body">
-        <img src="${data.image}" class="card-img-top" alt="...">
-        <p id="release" class="card-text">${data.releaseDate ? data.releaseDate : ''}</p>
+        <img src="${data.image}" class="card-img-top w-50 img-fluid" alt="...">
+        <p id="release" class="card-text">${data.releaseDate ? data.releaseDate : 'Releasing Soon'}</p>
             <h6 class="card-title">${data.name}</h6>
             <p class="card-text">Brand:${data.brand}</p>
             <p class="card-text">Storage:${data.mainFeatures.storage}</p>
             <p class="card-text">DisplaySize:${data.mainFeatures.displaySize}</p>
             <p class="card-text">Memory:${data.mainFeatures.memory}</p>
             <p class="card-text">Sensors:${data.mainFeatures.sensors.join()}</p>
-            
+        </div>
+            </div>
+            <div class="col-lg-12 ">
+         <div id="others" class="  card-body " ${data.others ? data.others : ""}>
             <hr class="w-100">
-            <p class="card-text">WLAN:${data.others.WLAN ? data.others.WLAN : ''}</p>
-            <p class="card-text">Bluetooth:${data.others.Bluetooth ? data.others.Bluetooth : ''}</p>
-            <p class="card-text">GPS:${data.others.GPS ? data.others.GPS : ''}</p>
-            <p class="card-text">NFC:${data.others.NFC ? data.others.NFC : ''}</p>
-            <p class="card-text">USB:${data.others.USB ? data.others.USB : ''}</p>
-            <p class="card-text">Radio:${data.others.Radio ? data.others.Radio : ''}</p>
+
+            <h6 id="others"class="card-title"></h6>
+            <p class="card-text">WLAN:${data.others.WLAN}</p>
+            <p class="card-text">Bluetooth:${data.others.Bluetooth}</p>
+            <p class="card-text">GPS:${data.others.GPS}</p>
+            <p class="card-text">NFC:${data.others.NFC}</p>
+            <p class="card-text">USB:${data.others.USB}</p>
+            <p class="card-text">Radio:${data.others.Radio}</p>
+         </div>
+        </div>
 
 
         
